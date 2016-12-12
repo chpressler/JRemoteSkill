54215535d2deb77f5945aa8723947618a2e651b4
Christian Pressler <christian.pressler@gmail.com>
2016-12-13 00:30:37 +0100

    initial commit

---
+++ src/main/java/com/jensui/RaspiRemoteSkill.java
@@ -0,0 +1,28 @@
+package com.jensui;
+
+import com.amazon.speech.speechlet.*;
+
+/**
+ * Created by christian on 11/30/16.
+ */
+public class RaspiRemoteSkill implements Speechlet {
+
+    public void onSessionStarted(SessionStartedRequest sessionStartedRequest, Session session) throws SpeechletException {
+
+    }
+
+    public SpeechletResponse onLaunch(LaunchRequest launchRequest, Session session) throws SpeechletException {
+
+        return null;
+    }
+
+    public SpeechletResponse onIntent(IntentRequest intentRequest, Session session) throws SpeechletException {
+
+        return null;
+    }
+
+    public void onSessionEnded(SessionEndedRequest sessionEndedRequest, Session session) throws SpeechletException {
+
+    }
+
+}
